b4b46f713291994804a34db32bec21b97f264463
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-11 23:00:46 +0530

    Edited version

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,20 @@ const App = () => {
 
   let [count, setCount] = useState(0)
 
+  const handleAlert = () => {
+    alert("cant edit it");
+  }
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  }
   return (
     <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
-    </div>
+      {/* <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1> */}
+      <h1 class="count" onDoubleClick={handleAlert}>{count}</h1>
+      {/* <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button> */}
+      <button class='increment-button' onClick={handleIncrement}>Increment</button>
+    </div >
   )
 }
 
